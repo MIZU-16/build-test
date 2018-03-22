dab288e32fc1f7b14da062ba389a7d6df0548ae7
shuhei-tada <shuhei-tada@altplus.co.jp>
2018-03-22 20:46:14 +0900

    editor追加

---
+++ Assets/Editor/AppBuilder.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class AppBuilder{
+
+    [MenuItem ("Build/Android向けビルド")]
+    static void buildForAndroid(){
+        bumpBuildNumberForAndroid ();
+        string errorMessage = BuildPipeline.BuildPlayer(GetAllScenePaths (), "/Users/plasticstraw/Desktop/game.apk", BuildTarget.Android, BuildOptions.None);
+        if( !string.IsNullOrEmpty( errorMessage ) )
+            Debug.LogError( "[Error!] " + errorMessage );
+        else
+            Debug.Log( "[Success!]" );
+    }
+
+    [MenuItem ("Build/iOS向けビルド")]
+    static void buildForIOS(){
+        bumpBuildNumberForIOS ();
+        // 出力パス。絶対パスで指定すること。また、最後にスラッシュを入れないこと。PostBuildProcess に渡る path が通常ビルドと異なってしまい、思わぬバグを引き起こすことがあります。
+        string path = "/Users/plasticstraw/Desktop/game";
+        string errorMessage = BuildPipeline.BuildPlayer(GetAllScenePaths(), path, BuildTarget.iOS, BuildOptions.Il2CPP);
+        if( !string.IsNullOrEmpty( errorMessage ) )
+            Debug.LogError( "[Error!] " + errorMessage );
+        else
+            Debug.Log( "[Success!]" );
+    }
+
+    static void bumpBuildNumberForIOS(){
+        string str = PlayerSettings.iOS.buildNumber;
+        int num = int.Parse (str);
+        num++;
+        PlayerSettings.iOS.buildNumber = num + "";
+    }
+
+    static void bumpBuildNumberForAndroid(){
+        PlayerSettings.Android.bundleVersionCode += 1;
+    }
+
+    static string[] GetAllScenePaths(){
+        string[] scenes = new string[EditorBuildSettings.scenes.Length];
+        for( int i = 0; i < EditorBuildSettings.scenes.Length ; i++ ) {
+            scenes[i] = EditorBuildSettings.scenes[i].path;
+        }
+        return scenes;
+    }
+}
---
+++ Assets/Editor/BuildBatch.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.Collections;
+
+public class BuildBatch : MonoBehaviour {
+
+    // build iOS app
+    [UnityEditor.MenuItem("Tools/Build Project AllScene iOS")]
+    private static void BuildiOS(){
+        Debug.Log("##########iOS Build Start#########");
+
+        //　全てのシーンを取得する
+        EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTargetGroup.iOS, BuildTarget.iOS );
+        string[] allScene = new string[EditorBuildSettings.scenes.Length];
+        int i = 0;
+        foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
+            Debug.Log ("scenePath(" + i + ")" + scene.path);
+            allScene[i] = scene.path;
+            i++;
+        }
+
+        // オプションの設定
+        BuildOptions opt = BuildOptions.SymlinkLibraries |
+        BuildOptions.AllowDebugging |
+        BuildOptions.ConnectWithProfiler |
+        BuildOptions.Development;
+
+        PlayerSettings.iOS.sdkVersion = iOSSdkVersion.DeviceSDK;
+        // Identifierは個人の環境に合わせる
+        PlayerSettings.applicationIdentifier = "jp.co.AnimeMiru";
+        // バージョンも必要に合わせて変更する
+        PlayerSettings.bundleVersion = "1.0.0";
+        PlayerSettings.statusBarHidden = true;
+        string errorMsg_Device = BuildPipeline.BuildPlayer( 
+            allScene,
+            "iOSDevice",
+            BuildTarget.iOS,
+            opt
+        );
+
+        if (string.IsNullOrEmpty(errorMsg_Device)){
+            Debug.Log ("##########Success iOS Device Build#########");
+        } else {
+            Debug.Log ("##########Failed iOS Device Build#########");
+            Debug.Log (errorMsg_Device);
+        }
+
+        //シュミレーター用
+        PlayerSettings.iOS.sdkVersion = iOSSdkVersion.SimulatorSDK;
+        string errorMsg_Simulator = BuildPipeline.BuildPlayer(
+            allScene, 
+            "Simulator", 
+            BuildTarget.iOS, 
+            opt
+        );
+        if (string.IsNullOrEmpty(errorMsg_Simulator)){
+            Debug.Log ("##########Success Simulator Build#########");
+        } else {
+            Debug.Log ("##########Failed Simulator Build#########");
+            Debug.Log (errorMsg_Device);
+        }
+
+    }
+}
